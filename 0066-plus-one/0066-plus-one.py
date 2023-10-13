880a1f4d1f518bb1c4d02832fc90f05d3009746d
Jailan Elsherbiny <125596530+jailan-sh@users.noreply.github.com>
2023-10-13 17:42:19 +0300

    Time: 34 ms (90.29%), Space: 16.2 MB (47.99%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        carry = 1
-        for i in range(len(digits)-1, -1, -1):
-            digits[i] += carry
-            carry = digits[i] // 10
-            digits[i] %= 10
-        if carry:
-            digits.insert(0, carry)
-        return digits
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return (digits)
+        return [1] + digits
+        
