8bab1c161a32251fb1ce081e9365d40895397d43
Jailan Elsherbiny <125596530+jailan-sh@users.noreply.github.com>
2023-10-18 23:05:02 +0300

    Time: 26 ms (25.03%), Space: 13.3 MB (87.15%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,7 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        a = sorted(nums)
-        return a[0]
-
-        
+        lo = 0
+        hi = len(nums) -1
+        if len(nums) == 1:
+            return nums[0]
+        if nums[lo] < nums[hi]:
+            return nums[lo]
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] < nums[mid+1] and nums[mid] < nums[mid -1]:
+                return nums[mid]
+            elif nums[mid] > nums[mid + 1]:
+                return nums[mid+1]
+            elif nums[mid] > nums[hi]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return -1
