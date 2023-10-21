14f2e9fdecb5875f41244c6b332e8ddd174e76fe
Jailan Elsherbiny <125596530+jailan-sh@users.noreply.github.com>
2023-10-22 01:42:44 +0300

    Time: 0 ms (100%), Space: 6.7 MB (55.63%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -6,8 +6,6 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    struct ListNode* two = list2;
-    struct ListNode* one = list1;
     struct ListNode* head;
     struct ListNode* dummy;
     if (list1 == NULL && list2 == NULL){
@@ -19,36 +17,36 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
     if (list2 == NULL){
     return (list1);
     }
-    if (one->val <= two->val)
+    if (list1->val <= list2->val)
     {
-        dummy = one;
-        one = one->next;
+        dummy = list1;
+        list1 = list1->next;
     }
     else{
-        dummy = two;
-        two = two->next;
+        dummy = list2;
+        list2 = list2->next;
     }
     head = dummy;
-    while(one && two)
+    while(list1 && list2)
     {
-        if (one->val <= two->val){
-            dummy->next = one;
-            dummy = one;
-            one = dummy->next;
+        if (list1->val <= list2->val){
+            dummy->next = list1;
+            dummy = list1;
+            list1 = dummy->next;
         }
         else
         {
-            dummy->next = two;
-            dummy = two;
-            two = dummy->next;
+            dummy->next = list2;
+            dummy = list2;
+            list2 = dummy->next;
         }
     }
-    if (one == NULL){
-        dummy->next = two;
+    if (list1 == NULL){
+        dummy->next = list2;
     }
     else
     {
-        dummy->next = one;
+        dummy->next = list1;
     }
     return (head);
 }
