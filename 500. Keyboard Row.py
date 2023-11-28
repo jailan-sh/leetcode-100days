432fab2a0e6a93448e651fb5f41f2e03bd09d018
Jailan Elsherbiny <125596530+jailan-sh@users.noreply.github.com>
2023-11-29 00:26:26 +0200

    improve

--- 500. Keyboard Row.py
+++ 500. Keyboard Row.py
@@ -4,29 +4,12 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        row1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
-        row2 = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
-        row3 = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+        rows = [set("qwertyuiop"),
+               set("asdfghjkl"),
+               set("zxcvbnm")]
         result = []
-        
         for word in words:
             new = word.lower()
-            if new[0] in row1:
-                for i in range(len(new)):
-                    if new[i] not in row1:
-                        break
-                else:
-                    result.append(word)
-            elif new[0] in row2:
-                for i in range(len(new)):
-                    if new[i] not in row2:
-                        break
-                else:
-                    result.append(word)
-            elif new[0] in row3:
-                for i in range(len(new)):
-                    if new[i] not in row3:
-                        break
-                else:
-                    result.append(word)
+            if any(set(new) <= row for row in rows):
+                result.append(word)
         return result
