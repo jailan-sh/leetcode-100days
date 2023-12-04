76204cab534fd3d155eb985e83a88904ea80beb4
Jailan Elsherbiny <125596530+jailan-sh@users.noreply.github.com>
2023-12-04 03:17:42 +0200

    algorithm

--- 442. Find All Duplicates in an Array.py
+++ 442. Find All Duplicates in an Array.py
@@ -12,3 +12,18 @@ class Solution(object):
             else:
                 result.append(abs(num))
         return result
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        duplicate = {}
+        result = []
+        for num in nums:
+            duplicate[num] = duplicate.get(num, 0) + 1
+        for k , v in duplicate.items():
+            if v == 2:
+                result.append(k)
+        return result
