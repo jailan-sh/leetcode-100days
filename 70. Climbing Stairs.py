37330dd343b3e59c60c455581cae186a7333ee18
Jailan Elsherbiny <125596530+jailan-sh@users.noreply.github.com>
2023-12-05 21:59:52 +0200

    DP easy

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        s1, s2 = 1, 1
+
+        for i in range(n-1):
+            temp = s1
+            s1 = s1 + s2
+            s2 = temp
+        return s1
