4886cced8d56bd1eb91e5440a08bc69d03d7cb19
Jailan Elsherbiny <125596530+jailan-sh@users.noreply.github.com>
2023-12-13 03:05:02 +0200

    stack

--- 844. Backspace String Compare.py
+++ 844. Backspace String Compare.py
@@ -19,3 +19,21 @@ class Solution(object):
         stack1 =[]
         stack2 = []
         return compare(stack1, s) == compare(stack2, t)
+
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        def compare(st):
+            stack = []
+            for i in range(len(st)):
+                if st[i] == '#':
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(st[i])
+            return ''.join(stack)
+        return compare(s) == compare(t)
